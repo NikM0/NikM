091dba4661019d22e98f2b939c5743cb310de5f5
NikM0 <nikhil.meon0@gmail.com>
2024-11-08 12:16:11 +1100

    Update script.js

--- script.js
+++ script.js
@@ -32,14 +32,14 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    const targetId = this.getAttribute('href').substring(1);
-    const targetSection = document.getElementById(targetId);
-
-    window.scrollTo({
-      top: targetSection.offsetTop,
-      behavior: 'smooth'
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        
+        targetSection.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
     });
-  });
 });
