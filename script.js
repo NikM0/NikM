66e34d7cf2befd66400ee2aef7ed44f28e0ba456
NikM0 <nikhil.meon0@gmail.com>
2024-12-12 21:30:01 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-window.addEventListener("load", function () {
-  // Glitch Effect
+document.addEventListener("DOMContentLoaded", function () {
   const glitchName = document.getElementById("glitch-name");
   const originalText = glitchName.textContent;
 
@@ -17,7 +16,7 @@ window.addEventListener("load", function () {
   }
 
   function triggerRandomGlitch() {
-    const randomDelay = Math.random() * 5000 + 4000; // Random delay between 4 to 9 seconds
+    const randomDelay = Math.random() * 5000 + 4000; // Random delay between 2 to 7 seconds
     setTimeout(() => {
       startGlitch();
       triggerRandomGlitch(); // Set up the next glitch after this one completes
@@ -30,27 +29,12 @@ window.addEventListener("load", function () {
     const chars = "[{}]-_=+1234567890!@#$%^&*()|\/;:,.<>~";
     return chars[Math.floor(Math.random() * chars.length)];
   }
+});
 
-  // Smooth scrolling for navigation without updating URL
-  document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault(); // Prevent default link behavior
-
-      // Get the target section
-      const targetId = this.getAttribute('href').substring(1); // Get the target section ID
-      const targetSection = document.getElementById(targetId);
-
-      // Scroll the section into the center of the screen
-      const sectionRect = targetSection.getBoundingClientRect();
-      const sectionTop = sectionRect.top + window.pageYOffset; // Get the section's top position relative to the page
-
-      const offset = (window.innerHeight - sectionRect.height) / 2; // Calculate the offset to center the section
-
-      // Scroll to the section with adjusted offset
-      window.scrollTo({
-        top: sectionTop - offset, // Adjusted scroll position
-        behavior: 'smooth' // Smooth scroll
-      });
+document.addEventListener("DOMContentLoaded", function () {
+    const scroll = new SmoothScroll('nav a[href*="#"]', {
+        speed: 800,
+        offset: 0,
+        updateURL: false
     });
-  });
 });
